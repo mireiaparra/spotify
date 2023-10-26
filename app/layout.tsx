63369ff2db42bc7637e59b008185ecf42b784be3
mireia <mirmalparra@gmail.com>
2023-10-26 13:06:26 +0200

    Add sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
+import Sidebar from '@/components/Sidebar'
 import './globals.css'
 
 const font = Figtree({ subsets: ['latin'] })
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Sidebar>
+        {children}
+        </Sidebar>
+        </body>
     </html>
   )
 }
